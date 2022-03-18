85854ab361d9e8879b1de018956f57a73a2edc0a
jacoobanderson <j00cwa@gmail.com>
2022-03-18 13:03:45 +0100

    Establish db connection

--- src/server.js
+++ src/server.js
@@ -1,49 +1,50 @@
 import express from 'express'
 import helmet from 'helmet'
 import logger from 'morgan'
+import { connectDB } from './config/mongoose.js'
 
 try {
-    await connectDB()
-  
-    const app = express()
+  await connectDB()
 
-    app.use(helmet())
+  const app = express()
 
-    app.use(logger('dev'))
+  app.use(helmet())
 
-    app.use(express.json())
+  app.use(logger('dev'))
 
-    // app.use('/', router)
-  
-    // Error handler.
-    app.use(function (err, req, res, next) {
-      err.status = err.status || 500
-  
-      if (req.app.get('env') !== 'development') {
-        return res
-          .status(err.status)
-          .json({
-            status: err.status,
-            message: err.message
-          })
-      }
-  
-      // Development only!
-      // Only providing detailed error in development.
+  app.use(express.json())
+
+  // app.use('/', router)
+
+  // Error handler.
+  app.use(function (err, req, res, next) {
+    err.status = err.status || 500
+
+    if (req.app.get('env') !== 'development') {
       return res
         .status(err.status)
         .json({
           status: err.status,
           message: err.message
         })
-    })
-  
-    // Starts the HTTP server listening for connections.
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running at http://localhost:${process.env.PORT}`)
-      console.log('Press Ctrl-C to terminate...')
-    })
-  } catch (err) {
-    console.error(err)
-    process.exitCode = 1
-  }  
+    }
+
+    // Development only!
+    // Only providing detailed error in development.
+    return res
+      .status(err.status)
+      .json({
+        status: err.status,
+        message: err.message
+      })
+  })
+
+  // Starts the HTTP server listening for connections.
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running at http://localhost:${process.env.PORT}`)
+    console.log('Press Ctrl-C to terminate...')
+  })
+} catch (err) {
+  console.error(err)
+  process.exitCode = 1
+}
