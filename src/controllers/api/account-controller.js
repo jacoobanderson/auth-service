f0bc85ac4c2b1d2d57855a1cd72520dda576a5c2
jacoobanderson <j00cwa@gmail.com>
2022-03-18 16:35:54 +0100

    Add register functionality in controller

--- src/controllers/api/account-controller.js
+++ src/controllers/api/account-controller.js
@@ -45,4 +45,44 @@ export class AccountController {
       next(err)
     }
   }
+
+  /**
+   * Registers a user.
+   *
+   * @param {object} req - Express request object.
+   * @param {object} res - Express response object.
+   * @param {Function} next - Express next middleware function.
+   */
+  async register (req, res, next) {
+    try {
+      const user = new User({
+        username: req.body.username,
+        password: req.body.password,
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        permissionLevel: 1
+      })
+
+      await user.save()
+
+      res
+        .status(201)
+        .json({ id: user.id })
+    } catch (error) {
+      let err = error
+
+      if (err.code === 11000) {
+        // Duplicated keys.
+        err = createError(409)
+        err.cause = error
+      } else if (error.name === 'ValidationError') {
+        // Validation error(s).
+        err = createError(400)
+        err.cause = error
+      }
+
+      next(err)
+    }
+  }
 }
